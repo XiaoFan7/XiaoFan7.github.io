324776d49e7aa35ff49f3abdc5692eac86933b95
XiaoFan7 <1093877136@qq.com>
2021-03-31 22:37:24 +0800

    Site updated: 2021-03-31 22:37:18

--- libs/codeBlock/codeLang.js
+++ libs/codeBlock/codeLang.js
@@ -1,3 +1,3 @@
-// build time:Mon Mar 29 2021 00:32:43 GMT+0800 (GMT+08:00)
+// build time:Wed Mar 31 2021 22:28:41 GMT+0800 (GMT+08:00)
 $(function(){var e=$('<div class="code_lang" title="代码语言"></div>');$("pre").before(e);$("pre").each(function(){var e=$(this).attr("class");if(!e){return true}var r=e.replace("line-numbers","").trim().replace("language-","").trim();$(this).siblings(".code_lang").text(r)})});
 //rebuild by neat 
