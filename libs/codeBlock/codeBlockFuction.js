c511a450cdc5de07335f239d19a143e7b0d01a2f
XiaoFan7 <1093877136@qq.com>
2021-08-21 21:16:20 +0800

    Site updated: 2021-08-21 21:16:15

--- libs/codeBlock/codeBlockFuction.js
+++ libs/codeBlock/codeBlockFuction.js
@@ -1,3 +1,3 @@
-// build time:Sun Aug 08 2021 03:43:25 GMT+0800 (GMT+08:00)
+// build time:Sat Aug 21 2021 21:16:07 GMT+0800 (GMT+08:00)
 $(function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')});
 //rebuild by neat 
--- libs/codeBlock/codeLang.js
+++ libs/codeBlock/codeLang.js
@@ -1,3 +1,3 @@
-// build time:Sun Aug 08 2021 03:43:25 GMT+0800 (GMT+08:00)
+// build time:Sat Aug 21 2021 21:16:07 GMT+0800 (GMT+08:00)
 $(function(){var e=$('<div class="code_lang" title="代码语言"></div>');$("pre").before(e);$("pre").each(function(){var e=$(this).attr("class");if(!e){return true}var r=e.replace("line-numbers","").trim().replace("language-","").trim();$(this).siblings(".code_lang").text(r)})});
 //rebuild by neat 
